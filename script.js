918661ee7eedfff6f8030e17576935414ffe817f
Maxim Skalkin <maxmlnsim@gmail.com>
2022-02-23 11:28:21 +0300

    New lesson

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-function FirstApp(name, age){
-    alert(`Hello m y name is ${name} and its my first app`);
-    function SkillApp(){
-        let skills = ['html', 'css', 'js'];
-        for (let i = 0; i < skills.length; i++){
-           alert(`There are my skills in programming ${skills[i]}`);
-        }
-    }
-    SkillApp();
-    function checkAge(age){
-        if (age > 18){
-            alert ("Nice to meet u, buddy");
-        } else {
-            alert ("This door are closed for u");
-        }
-    }
-    checkAge(22);
+let counter = 0;
+function onClickButton(el){
+    counter++;
+    el.innerHTML = "U pressed " + counter +" times";
+    el.style.background = "red";
+    el.style.color = "blue";
+    console.log(el.name);
+    el.style.cssText = "border-radius: 5px; border: 0; font-size:20px";
 }
-FirstApp("Ivan",22);
+
